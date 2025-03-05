748ccdfec8a08b84c7b3f59b10fb4a89a79fc0b1
ziaddorbuk <49367279+ziaddorbuk@users.noreply.github.com>
2025-03-05 02:43:56 +0200

    enhance GCP function security by enforcing logging, access controls, and service account invocations

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -7,6 +7,22 @@ provider "google" {
 resource "google_storage_bucket" "function_bucket" {
   name     = "${var.project_id}-function-source"
   location = var.region
+  
+  # Fix for CKV_GCP_62
+  logging {
+    log_bucket = "${var.project_id}-function-source-logs"
+  }
+  
+  # Fix for CKV_GCP_29
+  uniform_bucket_level_access = true
+  
+  # Fix for CKV_GCP_114
+  public_access_prevention = "enforced"
+  
+  # Fix for CKV_GCP_78
+  versioning {
+    enabled = true
+  }
 }
 
 # First Cloud Function
@@ -26,6 +42,9 @@ resource "google_cloudfunctions_function" "function1" {
   source_archive_object = google_storage_bucket_object.function1_source.name
   trigger_http         = true
   entry_point         = "main"
+  
+  # Fix for CKV_GCP_124
+  ingress_settings = "ALLOW_INTERNAL_ONLY"
 }
 
 # Second Cloud Function
@@ -45,21 +64,26 @@ resource "google_cloudfunctions_function" "function2" {
   source_archive_object = google_storage_bucket_object.function2_source.name
   trigger_http         = true
   entry_point         = "main"
+  
+  # Fix for CKV_GCP_124
+  ingress_settings = "ALLOW_INTERNAL_ONLY"
 }
 
-# IAM entries to make the functions publicly accessible
+# Replace public access with specific service accounts or groups
 resource "google_cloudfunctions_function_iam_member" "function1_invoker" {
   project        = google_cloudfunctions_function.function1.project
-  region        = google_cloudfunctions_function.function1.region
+  region         = google_cloudfunctions_function.function1.region
   cloud_function = google_cloudfunctions_function.function1.name
-  role          = "roles/cloudfunctions.invoker"
-  member        = "allUsers"
+  role           = "roles/cloudfunctions.invoker"
+  # Fix for CKV_GCP_107
+  member         = "serviceAccount:${var.service_account_email}"
 }
 
 resource "google_cloudfunctions_function_iam_member" "function2_invoker" {
   project        = google_cloudfunctions_function.function2.project
-  region        = google_cloudfunctions_function.function2.region
+  region         = google_cloudfunctions_function.function2.region
   cloud_function = google_cloudfunctions_function.function2.name
-  role          = "roles/cloudfunctions.invoker"
-  member        = "allUsers"
+  role           = "roles/cloudfunctions.invoker"
+  # Fix for CKV_GCP_107
+  member         = "serviceAccount:${var.service_account_email}"
 }
--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -14,3 +14,8 @@ variable "function_runtime" {
   type        = string
   default     = "python39"
 }
+
+variable "service_account_email" {
+  description = "The service account email that will be allowed to invoke the functions"
+  type        = string
+}
